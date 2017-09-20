322ce1e542bb5ae748b1f035017056669d6926de
Veli-Matti Aho <veli-matti.aho@kolumbus.fi>
2017-09-20 14:43:10 +0300

    Remove deprecated Override

--- android/src/main/java/com/reactlibrary/googlesignin/RNGoogleSignInPackage.java
+++ android/src/main/java/com/reactlibrary/googlesignin/RNGoogleSignInPackage.java
@@ -16,11 +16,6 @@ public class RNGoogleSignInPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNGoogleSignInModule(reactContext));
     }
 
-    // Depreciated RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
-
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
